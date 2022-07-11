d4b41b63479598b20dd74506362be1ab9e841383
Evangelos Karatzas <32259775+vagkaratzas@users.noreply.github.com>
2022-07-11 18:59:05 +0300

    Local VR mode disabled

--- ui.R
+++ ui.R
@@ -52,7 +52,7 @@ ui <- fluidPage(
                        checkboxInput("autoRotateScene", "Enable Scene Auto Rotate", F),
                        radioButtons("predefined_layout", "Select Predefined Layout:", c("Parallel Coordinates" = "parallel", "Zig Zag" = "zigZag", "Star" = "starLike", "Cube" = "cube") ),
                                div(id='sceneColorPicker', class='colorPicker'),
-                               actionButton("vr_button", "See in VR"),
+                               # actionButton("vr_button", "See in VR"),
                                actionButton("hideButton3", icon("angle-up"), class ="hideButton")
                       ),
                       tabPanel("Layer Actions",
